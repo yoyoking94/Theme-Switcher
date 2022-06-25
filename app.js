e3802e607627f92b54af44d1d876f06b56454020
Yovish MOONESAMY <56436435+yoyoking94@users.noreply.github.com>
2022-06-26 00:15:42 +0200

    update js

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 function changeTheme() {
     const nav = document.querySelector('nav')
     const input = document.querySelector('#switch')
-
     const elements = document.querySelectorAll("a");
-    elements.forEach((element) => (element.style.color = "#fff"));
 
+    elements.forEach((element) => (element.style.color = "#fff"));
     document.querySelector('header').style.color = "#fff";
 
     input.addEventListener('change', () => {
         if (input.checked) {
             console.log("Dark side !");
 
-            const elements = document.querySelectorAll("a");
             elements.forEach((element) => (element.style.color = "#ffffff"));
+
             document.querySelector('*').style.color = "#ffffff";
             document.querySelector('nav').style.backgroundColor = "#333333";
+            document.querySelector('nav').style.color = "#fff";
             document.querySelector('body').style.backgroundColor = "#5c5c5c";
             document.querySelector('footer').style.backgroundColor = "#333333";
         } else {
             console.log("The force !");
 
-            const elements = document.querySelectorAll("a");
             elements.forEach((element) => (element.style.color = "#333333"));
+
             document.querySelector('*').style.color = '#333333';
             document.querySelector('nav').style.backgroundColor = "#f5f5f5";
+            document.querySelector('nav').style.color = "#333";
             document.querySelector('body').style.backgroundColor = "#ffffff";
             document.querySelector('footer').style.backgroundColor = "#f5f5f5";
         }
